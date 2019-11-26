05f9c99f41f52cb8c16c5ef2db1c63c2584ffa26
Rb0ss <42614757+Rb0ss@users.noreply.github.com>
2019-11-26 19:41:07 +0000

    Add buttons
    yes

--- Main_File.v
+++ Main_File.v
@@ -1,7 +1,5 @@
-`timescale 1ns / 1ps
-
-
 module Main_File( input Master_Clock_In, Reset_N_In,
+                  input BTNU, BTND, BTNL, BTNR,
                   output [3:0] Main_Red_Out, Main_Green_Out, Main_Blue_Out,
                   output Sync_Horiz_Out, Sync_Vert_Out);
    
@@ -10,7 +8,7 @@ module Main_File( input Master_Clock_In, Reset_N_In,
    wire [9:0] Val_Column, Val_Row;
    
    Clock_Div M1 (
-    .Master_Clock_In(Master_Clock_In), .Clock_Out(Clock_25MHz));
+    .Master_Clock_In(Master_Clock_In), .Clock_Out(Clock_25MHz)); 
    
    VGA_Control M2(
         .Master_Clock_In(Clock_25MHz), .Reset_N_In(Reset_N_In), 	
@@ -20,6 +18,7 @@ module Main_File( input Master_Clock_In, Reset_N_In,
             
     VGA_Draw M3(
     	.Master_Clock_In(Clock_25MHz), .Reset_N_In(Reset_N_In),
+    	.Up(BTNU), .Down(BTND), .Left(BTNL), .Right(BTNR),
         .Disp_Ena_In(Disp_Enable),
 	    .Val_Col_In(Val_Column), .Val_Row_In(Val_Row),
 	    .Red(Main_Red_Out), .Blue(Main_Blue_Out), .Green(Main_Green_Out));
